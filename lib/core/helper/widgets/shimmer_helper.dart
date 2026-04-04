6b00e63fd78bf0277f82ca009b70afd8650502aa
Ahmed <gg@.g>
2026-04-04 07:22:26 +0200

    shimmer dark mode support

--- lib/core/helper/widgets/shimmer_helper.dart
+++ lib/core/helper/widgets/shimmer_helper.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerHelper {
-  static final Color _shimmerBase = Colors.grey.shade200;
-  static final Color _shimmerHighlighted = Colors.grey.shade300;
+  static Color get _shimmerBase => AppColors.isDark ? Colors.grey.shade800 : Colors.grey.shade200;
+
+  static Color get _shimmerHighlighted => AppColors.isDark ? Colors.grey.shade700 : Colors.grey.shade300;
 
   static Widget buildBasicShimmer({double height = double.infinity, double width = double.infinity, double radios = 0}) {
     return ClipRRect(
